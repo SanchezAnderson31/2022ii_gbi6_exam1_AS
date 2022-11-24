323a5b0eae35d392397932c3702133dc35b1a60e
Anderson Sanchez <anderson.sanchez@est.ikiam.edu.ec>
2022-11-23 20:23:46 -0500

    Cambios realizados en bacteria

--- code/bacteria.sh
+++ code/bacteria.sh
@@ -1,2 +1,7 @@
-.
+echo "Numero de registros "
+cut -d "," -f 2 ../data/grampa.csv | uniq | wc -c
+echo "conteo mas alto"
+cut -d "," -f 2 ../data/grampa.csv | uniq -c | sort -n | head -n 1
+echo "conteo mas bajo"
+cut -d "," -f 2 ../data/grampa.csv | uniq -c | sort -n | tail -n 1
 
