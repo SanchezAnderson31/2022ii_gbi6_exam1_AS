e7c0721c6dd530020e5f310f369d1f4bd216ae87
Anderson Sanchez <anderson.sanchez@est.ikiam.edu.ec>
2022-11-23 22:06:49 -0500

    se realizaron cambios en bacteria y secuencia

--- code/bacteria.sh
+++ code/bacteria.sh
@@ -1,7 +1,7 @@
 echo "Numero de registros "
-cut -d "," -f 2 ../data/grampa.csv | uniq | wc -c
+cut -d "," -f 2 ../data/grampa.csv | tail -n +2 | sort | uniq -c | sort -n
 echo "conteo mas alto"
-cut -d "," -f 2 ../data/grampa.csv | uniq -c | sort -n | head -n 1
+cut -d "," -f 2 ../data/grampa.csv | tail -n +2 | sort | uniq -c | sort -n | tail -n 1
 echo "conteo mas bajo"
-cut -d "," -f 2 ../data/grampa.csv | uniq -c | sort -n | tail -n 1
+cut -d "," -f 2 ../data/grampa.csv | tail -n +2 | sort | uniq -c | sort -n | head -n 1
 
--- code/secuencia.sh
+++ code/secuencia.sh
@@ -1,2 +1,6 @@
-.
+echo "numero de cantidad de cada peptido"
+cut -d "," -f 4 ../data/grampa.csv | tail -n +2 | sort | uniq -c | sort -n >> ../data/amp.txt
+echo "cantidad de dominio WGS"
+cut -d "," -f 4 ../data/grampa.csv | tail -n +2 | grep WGS | wc -l >> ../data/wgs.cvs
+
 
